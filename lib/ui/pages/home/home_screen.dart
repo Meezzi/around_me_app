393a7d6bd2a1ffde5641a4dedcf946e7e6378aff
Meezzi <alswl-3016@naver.com>
2025-04-17 14:36:51 +0900

    feat: HomeScreen에 장소 카드 리스트 구현

--- lib/ui/pages/home/home_screen.dart
+++ lib/ui/pages/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:around_me_app/ui/pages/home/widgets/place_info_list.dart';
 import 'package:around_me_app/ui/pages/home/widgets/place_search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,13 @@ class HomeScreen extends StatelessWidget {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: Scaffold(appBar: AppBar(title: PlaceSearchTextField())),
+      child: Scaffold(
+        appBar: AppBar(title: PlaceSearchTextField()),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: PlaceList(),
+        ),
+      ),
     );
   }
 }
