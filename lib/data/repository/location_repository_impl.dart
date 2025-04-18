f7e54b4402f326e8d1cb79beabe1ef8c4e7ae4ca
Meezzi <alswl-3016@naver.com>
2025-04-18 16:53:15 +0900

    feat: Naver Search API에 GET 요청

---
+++ lib/data/repository/location_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:around_me_app/data/model/place.dart';
+import 'package:around_me_app/data/repository/repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class LocationRepositoryImpl implements Repository {
+  final dio = Dio();
+  @override
+  Future<List<Place>> searchPlace(String query) async {
+    final response = await dio.get(
+      'https://openapi.naver.com/v1/search/local.json',
+      queryParameters: {'query': query, 'displasy': 5},
+      options: Options(
+        headers: {
+          'X-Naver-Client-Id': dotenv.env['NAVER_CLIENT_ID'],
+          'X-Naver-Client-Secret': dotenv.env['NAVER_CLIENT_SECRET'],
+        },
+      ),
+    );
+
+    return [];
+  }
+}
