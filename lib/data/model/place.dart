5e02c2f6000482175145c07e7b1e00ae883c575f
Meezzi <alswl-3016@naver.com>
2025-04-18 10:40:58 +0900

    feat: Place Model 클래스 생성

---
+++ lib/data/model/place.dart
@@ -0,0 +1,11 @@
+class Place {
+  String title;
+  String category;
+  String roadAddress;
+
+  Place({
+    required this.title,
+    required this.category,
+    required this.roadAddress,
+  });
+}
