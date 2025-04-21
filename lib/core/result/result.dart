c201dcf2f5add83c43a50877c42ada9fa6b562ee
Meezzi <alswl-3016@naver.com>
2025-04-21 11:40:53 +0900

    feat: Result 클래스 생성

---
+++ lib/core/result/result.dart
@@ -0,0 +1,21 @@
+import 'package:around_me_app/core/error/custom_exception.dart';
+
+sealed class Result<T> {
+  const Result();
+
+  factory Result.ok(T value) => Ok(value);
+
+  factory Result.error(CustomException error) => Error(error);
+}
+
+final class Ok<T> extends Result<T> {
+  const Ok(this.value);
+
+  final T value;
+}
+
+final class Error<T> extends Result<T> {
+  const Error(this.error);
+
+  final Exception error;
+}
