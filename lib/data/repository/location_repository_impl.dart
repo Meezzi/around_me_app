d39b01adeca4b1d0a34ff7807e24c8d4e6f41ad8
Meezzi <alswl-3016@naver.com>
2025-04-21 13:17:04 +0900

    feat: try-catch문으로 오류 처리

--- lib/data/repository/location_repository_impl.dart
+++ lib/data/repository/location_repository_impl.dart
@@ -17,36 +17,35 @@ class LocationRepositoryImpl implements LocationRepository {
 
   @override
   Future<Result<List<Place>>> searchPlace(String query) async {
-    final response = await dio.get(
-      'https://openapi.naver.com/v1/search/local.json',
-      queryParameters: {'query': query, 'display': 5},
-      options: Options(
-        headers: {
-          'X-Naver-Client-Id': naverClientId,
-          'X-Naver-Client-Secret': naverClientSecret,
-        },
-      ),
-    );
+    try {
+      final response = await dio.get(
+        'https://openapi.naver.com/v1/search/local.json',
+        queryParameters: {'query': query, 'display': 5},
+        options: Options(
+          headers: {
+            'X-Naver-Client-Id': naverClientId,
+            'X-Naver-Client-Secret': naverClientSecret,
+          },
+        ),
+      );
 
-    if (response.statusCode == 200) {
-      final List<Map<String, dynamic>> items = response.data['items'];
-      List<Place> itemList =
-          items.map((e) {
-            return Place(
-              title: e['title'],
-              category: e['category'],
-              roadAddress: e['roadAddress'],
-            );
-          }).toList();
+      if (response.statusCode == 200) {
+        final List<Map<String, dynamic>> items = response.data['items'];
+        List<Place> itemList = items.map((e) => Place.fromJson(e)).toList();
 
-      return Result.ok(itemList);
+        return Result.ok(itemList);
+      }
+
+      return Result.error(
+        CustomException(
+          errorCode: response.statusCode!,
+          errorMessage: response.statusMessage!,
+        ),
+      );
+    } catch (exception) {
+      return Result.error(
+        CustomException(errorMessage: exception.toString(), errorCode: null),
+      );
     }
-
-    return Result.error(
-      CustomException(
-        errorCode: response.statusCode!,
-        errorMessage: response.statusMessage!,
-      ),
-    );
   }
 }
