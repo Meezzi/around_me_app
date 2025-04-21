0599df06c0a930fa4e1286da57321e78f3b97e90
Meezzi <alswl-3016@naver.com>
2025-04-21 16:49:23 +0900

    feat: ConsumerWidget으로 변경

--- lib/ui/pages/home/widgets/place_info_list.dart
+++ lib/ui/pages/home/widgets/place_info_list.dart
@@ -1,11 +1,12 @@
 import 'package:around_me_app/ui/pages/home/widgets/place_info_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlaceList extends StatelessWidget {
+class PlaceList extends ConsumerWidget {
   const PlaceList({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ListView.separated(
       itemCount: 2,
       separatorBuilder: (context, index) => SizedBox(height: 12),
