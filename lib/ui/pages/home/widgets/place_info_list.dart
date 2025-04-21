56b503d53af7a700622c8726ccdc523aeb0925d7
Meezzi <alswl-3016@naver.com>
2025-04-21 16:50:36 +0900

    feat: HomeViewModel 상태 변경 시 PlaceList 자동 업데이트

--- lib/ui/pages/home/widgets/place_info_list.dart
+++ lib/ui/pages/home/widgets/place_info_list.dart
@@ -1,3 +1,4 @@
+import 'package:around_me_app/ui/pages/home/home_view_model.dart';
 import 'package:around_me_app/ui/pages/home/widgets/place_info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,11 +8,14 @@ class PlaceList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final places = ref.watch(homeViewModelProvider);
+
     return ListView.separated(
-      itemCount: 2,
+      itemCount: places.length,
       separatorBuilder: (context, index) => SizedBox(height: 12),
       itemBuilder: (context, index) {
-        return PlaceInfoCard();
+        final place = places[index];
+        return PlaceInfoCard(place: place);
       },
     );
   }
