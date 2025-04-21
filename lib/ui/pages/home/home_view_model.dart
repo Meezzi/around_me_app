984a94fcd26883360df3e4801000d3657a4912bf
Meezzi <alswl-3016@naver.com>
2025-04-21 16:46:41 +0900

    feat: HomeViewModel searchPlace 메서드 구현

--- lib/ui/pages/home/home_view_model.dart
+++ lib/ui/pages/home/home_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:around_me_app/core/result/result.dart';
 import 'package:around_me_app/data/model/place.dart';
 import 'package:around_me_app/data/repository/location_repository.dart';
 import 'package:riverpod/riverpod.dart';
@@ -10,5 +11,19 @@ class HomeViewModel extends Notifier<List<Place>> {
   @override
   List<Place> build() {
     return [];
+  }
+
+  /// 1. 지역 검색
+  Future<List<Place>> searchPlace(String query) async {
+    final result = await locationRepository.searchPlace(query);
+    switch (result) {
+      case Ok<List<Place>>():
+        state = result.value;
+        return state;
+
+      case Error<List<Place>>():
+        state = [];
+        return [];
+    }
   }
 }
