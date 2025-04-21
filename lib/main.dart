9f88a47b0bc15386c3e36343b5719d0eaf594688
Meezzi <alswl-3016@naver.com>
2025-04-21 16:32:02 +0900

    feat: MyApp을 ProviderScope로 래핑

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:around_me_app/theme/light.dart';
 import 'package:around_me_app/ui/pages/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(const MyApp());
-
   await dotenv.load(fileName: ".env");
+
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
