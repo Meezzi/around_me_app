54097e81909a0c3d675cda3384d77e19ff139f82
Meezzi <alswl-3016@naver.com>
2025-04-22 09:49:08 +0900

    feat: gps 버튼 터치 시 주소 조회

--- lib/ui/pages/home/home_screen.dart
+++ lib/ui/pages/home/home_screen.dart
@@ -1,10 +1,17 @@
+import 'package:around_me_app/ui/pages/home/home_view_model.dart';
 import 'package:around_me_app/ui/pages/home/widgets/place_info_list.dart';
 import 'package:around_me_app/ui/pages/home/widgets/place_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -12,11 +19,20 @@ class HomeScreen extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(title: PlaceSearchTextField()),
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: PlaceList(),
+        appBar: AppBar(
+          title: PlaceSearchTextField(),
+          actions: [
+            IconButton(
+              onPressed: () {
+                ref
+                    .read(homeViewModelProvider.notifier)
+                    .searchByCurrentLocation();
+              },
+              icon: Icon(Icons.gps_fixed),
+            ),
+          ],
         ),
+        body: Padding(padding: const EdgeInsets.all(12.0), child: PlaceList()),
       ),
     );
   }
