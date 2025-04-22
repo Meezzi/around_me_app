1e6437d9ddefb2ff54e74f03641ad6e6c708fe06
Meezzi <alswl-3016@naver.com>
2025-04-22 09:48:18 +0900

    feat: 현재 위치를 가져와서 주소를 불러온 후, 주소로 검색

--- lib/ui/pages/home/home_view_model.dart
+++ lib/ui/pages/home/home_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:around_me_app/core/geolocator/geolocator_helper.dart';
 import 'package:around_me_app/core/result/result.dart';
 import 'package:around_me_app/data/model/place.dart';
 import 'package:around_me_app/data/repository/location_repository.dart';
@@ -29,6 +30,40 @@ class HomeViewModel extends Notifier<List<Place>> {
         return [];
     }
   }
+
+  // 2. 현재 위치로 주소 조회
+  Future<void> searchByCurrentLocation() async {
+    // 현재 위치
+    final position = await GeolocatorHelper.getPosition();
+
+    // 위도, 경도로 주소 불러오기
+    final result = await locationRepository.findByLatLng(
+      position.latitude,
+      position.longitude,
+    );
+
+    // 응답 Result에 따른 분기 처리
+    switch (result) {
+      case Ok<List<String>>():
+        List<Place> allPlaces = [];
+        final addresses = result.value;
+
+        for (final address in addresses) {
+          final placeResult = await locationRepository.searchPlace(address);
+
+          if (placeResult is Ok<List<Place>>) {
+            allPlaces.addAll(placeResult.value);
+          }
+        }
+
+        state = allPlaces;
+        break;
+
+      case Error<List<String>>():
+        state = [];
+        break;
+    }
+  }
 }
 
 /// HomeViewModel 관리자
@@ -37,6 +72,7 @@ final homeViewModelProvider = NotifierProvider<HomeViewModel, List<Place>>(() {
     dio: Dio(),
     naverClientId: dotenv.env['NAVER_CLIENT_ID']!,
     naverClientSecret: dotenv.env['NAVER_CLIENT_SECRET']!,
+    vworldKey: dotenv.env['VWORLD_KEY']!,
   );
 
   return HomeViewModel(locationRepository);
